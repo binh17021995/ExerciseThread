0668fd526dba8a2167c7afdc9b29aa1c872fb97b
binh17021995 <binh.td95@gmail.com>
2022-08-08 11:43:17 +0700

    Sửa lại tính năng

--- app/src/main/java/com/monstarlab/exercisethread/MainActivity.kt
+++ app/src/main/java/com/monstarlab/exercisethread/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.monstarlab.exercisethread
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
@@ -11,6 +13,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,15 +23,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnPlus: Button
     private lateinit var tvInfor: TextView
     private lateinit var mHandler: Handler
-    var y1: Float = 0F
-    var y2: Float = 0F
+    private var y1: Float = 0F
+    private var y2: Float = 0F
+    private val rnd = Random()
+    private var color :Int =0
 
     companion object {
         const val MSG_UPDATE_NUMBER: Int = 100
         const val MSG_STOP_UPDATE_NUMBER: Int = 101
+        const val MIN_DISTANCE: Int = 200
     }
 
-    var number: Int = 0
+    private var number: Int = 0
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,117 +44,134 @@ class MainActivity : AppCompatActivity() {
         btnPlus = findViewById(R.id.btn_plus)
         tvInfor = findViewById(R.id.tv_infor)
         listenerHandler()
-        var time: Long = 1000
-        Thread.sleep(1000)
 
-        btnMinus.setOnTouchListener { v, event ->
-            btnMinus.onTouchEvent(event)
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    isKeep = true
-                    isUpdate = false
-                    minusThread(20)
-                    Log.i("BBB", "action down: ")
+        btnMinus.setOnClickListener {
+            color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
+            number--
+            tvInfor.text = "$number"
+            checkFinish.cancel()
+            checkFinish.start()
+        }
+        btnPlus.setOnClickListener {
+            color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
+            number++
+            tvInfor.text = "$number"
+            checkFinish.cancel()
+            checkFinish.start()
+        }
 
-                }
-                MotionEvent.ACTION_UP -> {
-                    isKeep = false
-                    time =20
-                    updateUI(20)
-                    Log.i("BBB", "action up: ")
-
-                }
-            }
-            true
+        btnMinus.setOnLongClickListener {
+            color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
+            isKeep = true
+            minusThread()
+            Log.i("BBB", " check Long: ")
+            checkFinish.cancel()
+            Log.i("BBB", " Tại đây diễn ra tn: ")
+            false
+        }
+        btnPlus.setOnLongClickListener {
+            color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
+            isKeep = true
+            plusThread()
+            Log.i("BBB", " check Long Plus: ")
+            checkFinish.cancel()
+            Log.i("BBB", " Tại đây diễn ra tn: ")
+            false
         }
 
 
-        btnPlus.setOnTouchListener { v, event ->
-            btnPlus.onTouchEvent(event)
+        tvInfor.setOnTouchListener { _, event ->
+            tvInfor.onTouchEvent(event)
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    isKeep = true
-                    isUpdate = false
-                     plusThread(20)
-                    Log.i("BBB", "action down: ")
+                    color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
+                    y1 = event.y
+                    Log.i("BBB", " UP")
 
                 }
                 MotionEvent.ACTION_UP -> {
+                    color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
                     isKeep = false
-                    time =20
-                    updateUI(20)
-                    Log.i("BBB", "action up: ")
+                    isUpdate = true
+                    mHandler.postDelayed({
+                        descendNumber()
+                    },2000)
+
+                    Log.i("BBB", " UP")
 
                 }
-            }
-            true
-        }
-        tvInfor.setOnTouchListener { v, event -> tvInfor.onTouchEvent(event)
-        when(event.action){
-            MotionEvent.ACTION_DOWN -> {
-                y1 =event.y
-                Log.i("BBB", " UP")
-
-            }
-            MotionEvent.ACTION_UP -> {
-                isKeep = false
-                isUpdate = true
-                updateUI(10)
-                Log.i("BBB", " UP")
-
-            }
-            MotionEvent.ACTION_MOVE -> {
-                y2 = event.y
-
-                    if(y1>y2){
-                        isKeep = true
-                        plusThread(20)
-                    } else {
-                        isKeep = true
-                        minusThread(20)
+                MotionEvent.ACTION_MOVE -> {
+                    color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(200),rnd.nextInt(300))
+                    y2 = event.y
+                    val valueY: Float = y2 - y1
+                    if (abs(valueY) > MIN_DISTANCE) {
+                        if (y2 > y1) {
+                            isKeep = true
+                            minusThread()
+                        } else {
+                            isKeep = true
+                            plusThread()
+                        }
                     }
-                Log.i("BBB", "MOVE $y2 $y1")
+                    Log.i("BBB", "MOVE $y2 $y1")
+
+                }
 
             }
-
-        }
 
             true
         }
 
     }
 
-    private fun minusThread(time : Long) {
+    private val checkFinish = object : CountDownTimer(2000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            Log.i("BBB", "vào đây không")
+            isKeep = false
+        }
+
+        override fun onFinish() {
+            Log.i("BBB", "Đây OnFinish")
+            isUpdate = true
+            descendNumber()
+        }
+    }
+
+
+    private fun minusThread() {
         Thread {
+
+            Log.i("BBB", "như tn")
             while (isKeep) {
                 number -= 1
                 val message = Message()
                 message.what = MSG_UPDATE_NUMBER
                 message.arg1 = number
+                message.arg2 = color
                 mHandler.sendMessage(message)
                 try {
-                    Thread.sleep(time)
+                    Thread.sleep(10)
                 } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
             }
+
             mHandler.sendEmptyMessage(MSG_STOP_UPDATE_NUMBER)
-
         }.start()
-
 
     }
 
-    private fun plusThread(time : Long) {
+    private fun plusThread() {
         Thread {
             while (isKeep) {
                 number += 1
                 val message = Message()
                 message.what = MSG_UPDATE_NUMBER
                 message.arg1 = number
+                message.arg2 = color
                 mHandler.sendMessage(message)
                 try {
-                    Thread.sleep(time)
+                    Thread.sleep(10)
                 } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
@@ -160,11 +185,11 @@ class MainActivity : AppCompatActivity() {
     private fun listenerHandler() {
         mHandler = object : Handler(Looper.getMainLooper()) {
             override fun handleMessage(msg: Message) {
-
                 when (msg.what) {
                     MSG_UPDATE_NUMBER -> {
                         isUpdate = true
-                        tvInfor.setText(msg.arg1.toString())
+                        tvInfor.text = msg.arg1.toString()
+                        tvInfor.setTextColor(msg.arg2)
                     }
                     MSG_STOP_UPDATE_NUMBER -> {
                         isKeep = false
@@ -175,31 +200,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun updateUI(time : Long) {
+    private fun descendNumber() {
         Thread {
-            Thread.sleep(1500)
             while (isUpdate && number != 0) {
                 if (number > 0) {
                     number -= 1
+                    Log.i("BBB", "descend")
                     val message = Message()
                     message.what = MSG_UPDATE_NUMBER
                     message.arg1 = number
+                    message.arg2 = color
                     mHandler.sendMessage(message)
                     try {
-                        Thread.sleep(time)
+                        Thread.sleep(10)
                     } catch (e: InterruptedException) {
                         e.printStackTrace()
                     }
 
                 } else {
-                    Thread.sleep(40)
                     number += 1
                     val message = Message()
                     message.what = MSG_UPDATE_NUMBER
                     message.arg1 = number
+                    message.arg2 = color
                     mHandler.sendMessage(message)
                     try {
-                        Thread.sleep(time)
+                        Thread.sleep(10)
                     } catch (e: InterruptedException) {
                         e.printStackTrace()
                     }
@@ -211,7 +237,6 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-
 
 
 }
